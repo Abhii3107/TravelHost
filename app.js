ebd6b2c674aca9c7c144004f928cccf7c22b21a8
Abhay Singh <abhaysingh03032002@gmail.com>
2025-07-29 01:12:26 +0530

    Create Review

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -135,6 +136,23 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
   res.redirect("/listings");
 }));
 
+//Reviews
+//Post Route
+app.post("/listings/:id/reviews" , async (req, res)=>{
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
+
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+
+  console.log("new review save");
+  res.redirect(`/listings/${listing._id}`);
+
+});
+
+
 // app.all("*",(req,res,next) =>{
 //   next( new ExpressError(404,"Page Not found !"));
 // });
@@ -156,4 +174,5 @@ npm i ejs
 npm i mongoose
 npm i method-override
 npm i ejs-mate
-*/
+
+npm  i joi*/
