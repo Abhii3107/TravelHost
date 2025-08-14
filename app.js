d32b47a376478ac4c2bd6a19cdd3fbce198d7b89
Abhay Singh <abhaysingh03032002@gmail.com>
2025-08-14 15:04:38 +0530

    test

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ passport.deserializeUser(User.deserializeUser());//fetch users full data from se
 app.use((req , res , next ) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user; 
+  res.locals.currUser = req.user || null; 
   next();
 })
 
